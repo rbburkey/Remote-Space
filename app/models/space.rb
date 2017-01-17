f2ac0cb989f93d1a9bebe61366586b983bef4346
rbburkey <robinsonbburkey@gmail.com>
2017-01-17 15:51:46 -0800

    search changes

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,12 @@
 class Space < ActiveRecord::Base
   has_many :reviews
   belongs_to :category
-  validates_presence_of :name, :category, :description, :address1, :city, :state, :zipcode, :phone, :website 
+  validates_presence_of :name, :category, :description, :address1, :city, :state, :zipcode, :phone, :website
+
+  def self.search(params)
+    spaces = Space.where(category_id: params[:category].to_i)
+    spaces = spaces.where("name like ? or description like ?", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
+    spaces = spaces.near(params[:location], 20) if params[:location].present?
+    spaces
+  end
 end
