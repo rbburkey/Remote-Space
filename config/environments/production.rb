7e462d4b5c5a0effe44b5cbd445b20115eab95cf
Robinson <robinsonbburkey@gmail.com>
2017-06-19 18:39:20 -0700

    s3 bucket name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ config.action_mailer.smtp_settings = {
 config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('remotespace.co'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
