25c3aee5b868445dce8c2dda3f8b129080af6c3e
Robinson <robinsonbburkey@gmail.com>
2017-07-11 15:28:08 -0700

    aws image display issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ config.action_mailer.smtp_settings = {
 
 config.paperclip_defaults = {
   storage: :s3,
+  s3_host_name: 's3-us-west-1.amazon.aws.com',
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
